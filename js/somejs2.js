4b8c3a41f8527335822fec51e87635109ae9c04d
Steven Shao <stevenshao1900@icloud.com>
2017-05-05 13:54:36 +0800

    more

--- js/somejs2.js
+++ js/somejs2.js
@@ -3,20 +3,30 @@
 //It is registered globally, so it can be used across all the root instances
 Vue.component('my-menubar', {
 	props:['menuitems'],
-	template: '<div><my-menuitem v-for="menuitem in menuitems" v-bind:menuitem="menuitem.name" v-bind:key="menuitem.id"></my-menuitem></div>',
+	template: '<div id="menubar"><my-menuitem v-for="menuitem in menuitems" v-bind:menuitem="menuitem" v-bind:key="menuitem.id"></my-menuitem></div>',
 	components:{
 		'my-menuitem':{
 			props: ['menuitem'],
-			template: '<div> {{menuitem}} </div>'
+			template: '<div class="menuitem"> {{menuitem}} </div>'
 		}
 	}
 });
+
+Vue.component('main-section',{
+    props:['bgPic','mainMsg'],
+    template: '<div id="mainsection"><slot>main section</slot></div>'
+
+});
+
 //root instance
 //data flow in from root down to children
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: "Hello Vue!",
+		message: "Hello Vue! This is the main message. I \
+        am trying to be very long. I am trying to be very long.\
+        I am trying to be very long. I am trying to be very long.",
+        message2: "This is a relatively short message. I show I am here.",
 		list:["duck","panda","monkey"],
         jobs:[]
 	},
@@ -30,6 +40,7 @@ var app = new Vue({
 
     methods:{
         fetchMenuList: function(){
+            // bind this
             var _this = this;
             axios.get('https://jsonplaceholder.typicode.com/users').then(function (response){
                 console.log("hello panda2");
