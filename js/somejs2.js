77f0e491fee7f29afef8778ac6c569c243542e85
Steven Shao <stevenshao1900@icloud.com>
2017-05-05 17:38:10 +0800

    more more

--- js/somejs2.js
+++ js/somejs2.js
@@ -27,8 +27,12 @@ var app = new Vue({
         am trying to be very long. I am trying to be very long.\
         I am trying to be very long. I am trying to be very long.",
         message2: "This is a relatively short message. I show I am here.",
+        message3: "This is statement. This is statement. This is statement.",
+        message4: "more messages, more messages, more messages, more messages, more messages, more messages,more messages, more messages, more messages",
+        titleMessage3: "THIS IS A TITLE",
 		list:["duck","panda","monkey"],
-        jobs:[]
+        jobs:[],
+        footmessage: "\u00A9 2017 LynxBerry"
 	},
 
     mounted: function(){
